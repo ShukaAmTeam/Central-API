a51120c0ab36c00c7758dc139e20f0703822614c
Grig <yeghiazaryan.grigor@iamcloud.com>
2016-07-29 15:28:07 +0400

    OrderRepository

---
+++ CentralApi/API.Data/DataAccess/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using API.Data.DataAccess.Repositories.EF;
+
+namespace API.Data.DataAccess
+{
+    public interface IOrderRepository : IRepository<Product>
+    {
+        IEnumerable<Order> GetTopOrders(int count);
+        IEnumerable<Order> GetOrders(int pageIndex, int pageSize);
+    }
+}
---
+++ CentralApi/API.Data/DataAccess/Repositories/OrderRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using API.Data.DataAccess.Repositories.EF;
+
+namespace API.Data.DataAccess.Repositories
+{
+    public class OrderRepository : Repository<Product>, IOrderRepository
+    {
+        /// <exception cref="ArgumentNullException"><paramref name="context" /> is <see langword="null" />.</exception>
+        public OrderRepository(CentralDbEntities context)
+            : base(context)
+        {
+        }
+
+        public CentralDbEntities CentralDbContext => DbContext as CentralDbEntities;
+
+        public IEnumerable<Order> GetOrders(int pageIndex, int pageSize)
+        {
+            return CentralDbContext
+                .Orders.Skip((pageIndex - 1)*pageSize)
+                .Take(pageSize);
+        }
+
+        public IEnumerable<Order> GetTopOrders(int count)
+        {
+            return CentralDbContext.Orders
+                .Take(count);
+        }
+    }
+}
